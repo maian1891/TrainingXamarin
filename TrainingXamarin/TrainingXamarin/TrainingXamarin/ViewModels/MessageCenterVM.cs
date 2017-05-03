ef4487fd4fee21a2c730119c53ec2924c20ea85a
maian1891 <maian1891@gmail.com>
2017-05-03 09:50:09 +0700

    Init TrainingXamarin project

---
+++ TrainingXamarin/TrainingXamarin/TrainingXamarin/ViewModels/MessageCenterVM.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainingXamarin.Views;
+using Xamarin.Forms;
+
+namespace TrainingXamarin.ViewModels
+{
+    public class MessageCenterVM
+    {
+        public ObservableCollection<string> Greetings { get; set; }
+
+        public MessageCenterVM()
+        {
+            Greetings = new ObservableCollection<string>();
+
+            MessagingCenter.Subscribe<MessageCenterV>(this, "Hi", (sender) => {
+                Greetings.Add("Hi");
+            });
+
+            MessagingCenter.Subscribe<MessageCenterV>(this, "Navigator", async (sender) => {
+                await App.Current.MainPage.Navigation.PushAsync(new TriggerPage());
+            });
+
+            MessagingCenter.Subscribe<MessageCenterV, string>(this, "Hi", (sender, arg) => {
+                Greetings.Add("Hi " + arg);
+            });
+        }
+    }
+}
---
+++ TrainingXamarin/TrainingXamarin/TrainingXamarin/Views/MessageCenterV.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TrainingXamarin.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TrainingXamarin.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MessageCenterV : ContentPage
+    {
+        public MessageCenterV()
+        {
+            InitializeComponent();
+
+            BindingContext = new MessageCenterVM();
+
+            // Subscribe to a message (which the ViewModel has also subscribed to) to pop up an Alert
+            MessagingCenter.Subscribe<MessageCenterV, string>(this, "Hi", (sender, arg) => {
+                DisplayAlert("Message Received", "arg=" + arg, "OK");
+            });
+
+            listView.SetBinding(ListView.ItemsSourceProperty, "Greetings");
+
+        }
+
+        private void Button_Clicked_Hi(object sender, EventArgs e)
+        {
+            MessagingCenter.Send<MessageCenterV>(this, "Hi");
+        }
+
+        private void Button_Clicked_HiJohn(object sender, EventArgs e)
+        {
+            MessagingCenter.Send<MessageCenterV, string>(this, "Hi", "John");
+        }
+
+        private void Button_Clicked_Unsubscribe(object sender, EventArgs e)
+        {
+            MessagingCenter.Unsubscribe<MessageCenterV, string>(this, "Hi");
+            DisplayAlert("Unsubscribed",
+                "This page has stopped listening, so no more alerts; however the ViewModel is still receiving messages.",
+                "OK");
+        }
+
+        private void Button_Clicked_Navigator(object sender, EventArgs e)
+        {
+            MessagingCenter.Send<MessageCenterV>(this, "Navigator");
+        }
+    }
+}
